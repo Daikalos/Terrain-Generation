8aa01925ccf145dbeca01ea449ecf5f6eaa6093c
Tobias G <31646748+Daikalos@users.noreply.github.com>
2021-10-12 11:19:23 +0200

    added alternative...

--- Assets/Scripts/HeatmapTexture.cs
+++ Assets/Scripts/HeatmapTexture.cs
@@ -8,21 +8,22 @@ public class HeatmapTexture : MonoBehaviour
     [SerializeField] private RenderTexture _RenderTexture;
     [SerializeField] private Renderer _Renderer;
     [Space(10)]
+    [SerializeField] private bool _UseAlternative;
     [SerializeField] private int _Samples;
     [SerializeField] private Gradient _HeatmapGradient;
 
     private Texture2D _Texture;
 
     private int[] _Heatmap;
-    private int _MaxHeat;
 
     private CustomNoiseParameters[] customNoises;
     private float[] _YCoords;
 
+    private float _OceanLevel = 0.30f;
+
     public void Run()
     {
         _Heatmap = new int[_RenderTexture.width * _RenderTexture.height];
-        _MaxHeat = -int.MaxValue;
 
         for (int i = 0; i < _Samples; ++i) // simulate creating new terrain
         {
@@ -45,7 +46,11 @@ public class HeatmapTexture : MonoBehaviour
             }
 
             SimulateTerrain(octaveCount, width, height, out float minValue, out float maxValue);
-            AddToHeatmap(width, height, minValue, maxValue);
+
+            if (_UseAlternative)
+                AddToHeatmapAlternative(width, height, minValue, maxValue);
+            else
+                AddToHeatmap(width, height, minValue, maxValue);
         }
 
         AssignTexture();
@@ -90,17 +95,97 @@ public class HeatmapTexture : MonoBehaviour
                 float vertexProc = Mathf.InverseLerp(min, max, _YCoords[i]);
 
                 linearity += vertexProc;
-                leniency += (vertexProc < 0.3f) ? 1.0f : 0.0f;
+                leniency += (vertexProc < _OceanLevel) ? 1.0f : 0.0f;
             }
         }
 
         linearity /= (width * height);
         leniency /= (width * height);
 
-        int pos = (int)(linearity * _RenderTexture.width - 1) + (int)(leniency * _RenderTexture.height - 1) * _RenderTexture.width;
+        int pos = (int)(linearity * _RenderTexture.width) + (int)(leniency * _RenderTexture.height) * _RenderTexture.width;
 
-        if (++_Heatmap[pos] > _MaxHeat)
-            _MaxHeat = _Heatmap[pos];
+        ++_Heatmap[pos];
+    }
+
+    private void AddToHeatmapAlternative(int width, int height, float min, float max)
+    {
+        double sumR2 = 0.0f;
+        int r2Count = 0;
+
+        for (int x = 0; x < width; ++x)
+        {
+            double sumZ = 0.0f;
+            double sumZ2 = 0.0f;
+            double sumY = 0.0f;
+            double sumY2 = 0.0f;
+            double sumZY = 0.0f;
+
+            for (int z = 0; z < height; ++z)
+            {
+                double y = _YCoords[x + z * width];
+
+                sumZ += z;
+                sumZ2 += z * z;
+                sumY += y;
+                sumY2 += y * y;
+                sumZY += z * y;
+            }
+
+            double r2 = System.Math.Pow((height * sumZY - sumZ * sumY) / System.Math.Sqrt((height * sumZ2 - sumZ * sumZ) * (height * sumY2 - sumY * sumY)), 2);
+
+            if (!double.IsNaN(r2))
+            {
+                sumR2 += r2;
+                ++r2Count;
+            }
+        }
+
+        for (int z = 0; z < height; ++z)
+        {
+            double sumX = 0.0f;
+            double sumX2 = 0.0f;
+            double sumY = 0.0f;
+            double sumY2 = 0.0f;
+            double sumXY = 0.0f;
+
+            for (int x = 0; x < width; ++x)
+            {
+                double y = _YCoords[x + z * width];
+
+                sumX += x;
+                sumX2 += x * x;
+                sumY += y;
+                sumY2 += y * y;
+                sumXY += x * y;
+            }
+
+            double r2 = System.Math.Pow((width * sumXY - sumX * sumY) / System.Math.Sqrt((width * sumX2 - sumX * sumX) * (width * sumY2 - sumY * sumY)), 2);
+
+            if (!double.IsNaN(r2))
+            {
+                sumR2 += r2;
+                ++r2Count;
+            }
+        }
+
+        if (r2Count == 0)
+            return;
+
+        double leniency = 0.0;
+        for (int x = 0; x < width; ++x)
+        {
+            for (int z = 0; z < height; ++z)
+            {
+                leniency += (Mathf.InverseLerp(min, max, _YCoords[x + z * width]) < _OceanLevel) ? 1.0 : 0.0;
+            }
+        }
+
+        double linearity = sumR2 / r2Count;
+        leniency /= (width * height);
+
+        int pos = (int)(linearity * _RenderTexture.width) + (int)(leniency * _RenderTexture.height) * _RenderTexture.width;
+
+        ++_Heatmap[pos];
     }
 
     private void AssignTexture()
@@ -115,7 +200,7 @@ public class HeatmapTexture : MonoBehaviour
         {
             for (int y = 0; y < _RenderTexture.height; ++y)
             {
-                float color = _Heatmap[x + y * _RenderTexture.width] / (float)_MaxHeat;
+                float color = _Heatmap[x + y * _RenderTexture.width] / 7.0f;
                 _Texture.SetPixel(y, (_RenderTexture.height - 1) - x, _HeatmapGradient.Evaluate(color));
             }
         }
